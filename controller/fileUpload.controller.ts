f3bae256ab04afbeed60cc652773f9161e01cfd0
ThoratSanchit <sanchitcode77@gmail.com>
2024-11-16 13:58:26 +0530

    new changes

--- controller/fileUpload.controller.ts
+++ controller/fileUpload.controller.ts
@@ -1,45 +1,45 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
-import fs from 'fs';
-import path from 'path';
-import { MultipartFile } from '@fastify/multipart';
+import { FastifyRequest, FastifyReply } from "fastify";
+import fs from "fs";
+import path from "path";
+import { MultipartFile } from "@fastify/multipart";
 
-const ALLOWED_EXTENSIONS = ['.jpg', '.jpeg', '.png', '.pdf', '.doc', '.heic', '.docx'];
-
-export const uploadFile = async (request: FastifyRequest, reply: FastifyReply) => {
+export const uploadFile = async (
+  request: FastifyRequest,
+  reply: FastifyReply
+) => {
   try {
-    // Get the file data from the request
     const data = await request.file();
 
     if (!data) {
-      return reply.status(400).send({ message: 'No file uploaded' });
+      return reply.status(400).send({ message: "No file uploaded" });
     }
 
-    // Restrict file types based on allowed extensions
     const extension = path.extname(data.filename).toLowerCase();
-    if (!ALLOWED_EXTENSIONS.includes(extension)) {
-      return reply.status(400).send({ message: 'Invalid file type. Only specific file types are allowed' });
+
+    if (extension === ".exe") {
+      return reply.status(400).send({
+        message: "Invalid file type. .exe file not allowed",
+      });
     }
+    const uploadPath = path.join(__dirname, "../uploads", data.filename);
 
-    // Define the upload path
-    const uploadPath = path.join(__dirname, '../uploads', data.filename);
-
-    // Write file to the server
     const fileStream = fs.createWriteStream(uploadPath);
     data.file.pipe(fileStream);
 
-    // Handle successful upload
-    fileStream.on('finish', () => {
-      reply.status(200).send({ message: 'File uploaded successfully', filename: data.filename });
+    fileStream.on("finish", () => {
+      reply.status(200).send({
+        message: "File uploaded successfully",
+        filename: data.filename,
+      });
     });
 
-    // Handle upload errors
-    // fileStream.on('error', (err) => {
-    //   console.error(err);
-    //   reply.status(500).send({ message: 'File upload failed' });
-    // });
-
+    reply.status(200).send({
+      message: "File uploaded successfully",
+      filename: data.filename,
+    });
+    
   } catch (err) {
     console.error(err);
-    reply.status(500).send({ message: 'File upload failed' });
+    reply.status(500).send({ message: "File upload failed" });
   }
 };
